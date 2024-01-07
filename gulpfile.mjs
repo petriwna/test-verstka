2246ba959e8456fb3be97b31f3cfb78437a4216e
petriwna <petriwna43@gmail.com>
2024-01-07 22:53:10 +0200

    fixed style table

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -11,7 +11,11 @@ import htmlmin from 'gulp-htmlmin';
 const paths = {
     styles: {
         src: 'src/style/**/*.scss',
-        dest: 'dist/style/'
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'dist/js/'
     },
     images: {
         src: 'src/public/images/**/*.{png,svg,jpg}',
@@ -32,6 +36,11 @@ gulp.task('html', () => {
     return gulp.src(paths.html.src)
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest(paths.html.dest))
+});
+
+gulp.task('scripts', () => {
+    return gulp.src(paths.scripts.src)
+        .pipe(gulp.dest(paths.scripts.dest))
 });
 
 gulp.task('sass', () => {
@@ -57,11 +66,12 @@ gulp.task('server', () => {
     });
 });
 
-gulp.task('default', gulp.series('html', 'sass', 'images', 'server'));
+gulp.task('default', gulp.series('html', 'sass', 'scripts', 'images', 'server'));
 
 gulp.task('watch', () => {
     gulp.watch(paths.html.src, gulp.series('html'));
     gulp.watch(paths.styles.src, gulp.series('sass'));
+    gulp.watch(paths.styles.src, gulp.series('scripts'));
     gulp.watch(paths.images.src, gulp.series('images'));
     gulp.watch('src/**/*', gulp.series('reload'));
 
@@ -71,3 +81,5 @@ gulp.task('reload', (done) => {
     connect.reload();
     done();
 });
+
+
